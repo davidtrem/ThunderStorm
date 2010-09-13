976972a849fc69101c26dcec8e3bc2f150f3e0a8
David Trémouilles <david.trem@gmail.com>
2010-09-13 15:24:35 +0200

    improved pulse observer

--- thunderstorm/lightning/pulse_observer.py
+++ thunderstorm/lightning/pulse_observer.py
@@ -35,12 +35,14 @@ class TLPPickFigure(object):
         curr = tlp_curve_data[1]
         tlp_plot.plot(volt, curr, '-')
         selected_flag = np.zeros(volt.shape[0], dtype=np.bool)
+        selected_flag[0] = True # set first one True
         points, = tlp_plot.plot(volt, curr, 'o', picker=5)
         points.identity = "Who am I?"
         selected_point, = tlp_plot.plot(volt[selected_flag],
-                                       curr[selected_flag], 'ro')
+                                        curr[selected_flag], 'ro')
         figure.canvas.mpl_connect('pick_event', self.onpickevent)
         figure.canvas.draw()
+        self.pulse_fig.update(selected_flag)
         self.figure = figure
         self.selected_flag = selected_flag
         self. selected_point = selected_point
@@ -53,9 +55,12 @@ class TLPPickFigure(object):
             selected_flag = self.selected_flag
             ind = event.ind
             selected_flag[ind] = not selected_flag[ind]
-            print 'onpick3 scatter'#, ind, np.take(x, ind), np.take(y, ind)
-            self.selected_point.set_data(self.volt[selected_flag], 
-                                         self.curr[selected_flag])
+            if not((-selected_flag).all()): # at least one true 
+                self.selected_point.set_data(self.volt[selected_flag], 
+                                             self.curr[selected_flag])
+                self.selected_point.set_visible(True)
+            else:
+                self.selected_point.set_visible(False)
             self.pulse_fig.update(selected_flag)
             self.figure.canvas.draw()  
             
@@ -69,25 +74,33 @@ class PulsesFigure(object):
         self.pulses = pulses
         # V curves
         v_pulse_plot = figure.add_subplot(211)
-        v_pulse_plot.grid(True)
-        v_pulse_plot.set_ylabel("Voltage")
-        v_pulse_plot.set_title(title)
         # I curves
         i_pulse_plot = figure.add_subplot(212, sharex=v_pulse_plot)
-        i_pulse_plot.grid(True)
-        i_pulse_plot.set_xlabel("time (ns)")
-        i_pulse_plot.set_ylabel("Current")
         figure.canvas.draw()
         self.figure = figure
         self.v_pulse_plot = v_pulse_plot
         self.i_pulse_plot = i_pulse_plot
         
     def update(self, selected_flag):
-        self.v_pulse_plot.clear()
-        self.v_pulse_plot.plot(self.offseted_time,
-                               self.pulses.voltage[selected_flag].T)
-        self.i_pulse_plot.clear()
-        self.i_pulse_plot.plot(self.offseted_time,
-                               self.pulses.current[selected_flag].T)
+        v_pulse_plot = self.v_pulse_plot
+        i_pulse_plot = self.i_pulse_plot
+        v_pulse_plot.hold(False)
+        i_pulse_plot.hold(False)
+        if not((-selected_flag).all()): # if at least one true...
+            v_pulse_plot.plot(self.offseted_time,
+                              self.pulses.voltage[selected_flag].T, 'b')
+            v_pulse_plot.grid(True)
+            v_pulse_plot.set_ylabel("Voltage")
+            v_pulse_plot.set_visible(True)
+            
+            i_pulse_plot.plot(self.offseted_time,
+                                   self.pulses.current[selected_flag].T, 'b')
+            i_pulse_plot.grid(True)
+            i_pulse_plot.set_xlabel("time (ns)")
+            i_pulse_plot.set_ylabel("Current")
+            i_pulse_plot.set_visible(True)
+        else:
+            v_pulse_plot.set_visible(False)
+            i_pulse_plot.set_visible(False)
         self.figure.canvas.draw()
         
