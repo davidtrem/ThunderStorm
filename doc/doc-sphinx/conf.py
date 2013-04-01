496f632d3b823fde9115fb065a6a757525a1da87
David Trémouilles <david.trem@gmail.com>
2013-04-01 22:54:36 +0300

    Update conf.py

--- doc/doc-sphinx/conf.py
+++ doc/doc-sphinx/conf.py
@@ -38,7 +38,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy', 'h5py', 'numpy.fft']
+MOCK_MODULES = ['numpy', 'h5py', 'numpy.fft', 'numpy.ma']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
