7b1fcddc4fe8f98afff97f7160ed5a3c307efbdc
David Trémouilles <david.trem@gmail.com>
2013-04-01 21:02:56 +0300

    Update conf.py

--- doc/doc-sphinx/conf.py
+++ doc/doc-sphinx/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ThunderStorm'
-copyright = u'2010, David Trémouilles'
+copyright = u'2013, David Trémouilles'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
