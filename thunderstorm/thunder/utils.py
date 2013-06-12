7f1ad0671dd29f700caa2bc254fe2a144a51dff6
David Trémouilles <david.trem@gmail.com>
2013-06-12 14:45:29 +0200

    py2_3 compatible utils

--- thunderstorm/thunder/utils.py
+++ thunderstorm/thunder/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import sys
 from io import StringIO
 
 
@@ -7,6 +8,10 @@ def string2file(raw_string):
     """The function return a file like object contaiing the given string.
     """
     filelike = StringIO()
-    filelike.write(raw_string)
-    filelike.reset()
+    if sys.version_info[0] < 3:  # Python 2
+        filelike.write(unicode(raw_string))
+    else:  # Python 3
+        filelike.write(raw_string)
+    filelike.flush()
+    filelike.seek(0)
     return filelike
